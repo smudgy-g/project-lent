7601554bec0132da95ca74b898299d68e9c964cc
Kevin Kowalski <hello@kevinkowalski.com>
2023-08-15 11:42:39 +0200

    feat: add CollectionSingle component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -36,6 +36,10 @@ const router = createBrowserRouter([
   {
     path: '/collections',
     element: <CollectionOverview />
+  },
+  {
+    path: '/collection/:collectionId',
+    element: <CollectionSingle />
   }
 ])
 
