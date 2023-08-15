f3b7d6ae6e49ce9acee5ec1098315cb0f55f005d
Kevin Kowalski <hello@kevinkowalski.com>
2023-08-15 14:41:54 +0200

    feat: embed TabNavigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import {AuthProvider, RequireAuth} from 'react-auth-kit';
 
 import Header from "./components/Header";
 import HeaderProvider from "./contexts/HeaderContext";
-// import TabNavigation from "./components/TabNavigation";
+import TabNavigation from "./components/TabNavigation";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Profile from "./components/profile/Profile";
@@ -17,46 +17,60 @@ import ProfileEdit from "./components/profile/ProfileEdit";
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (<RequireAuth loginPath='/login'>
-                <CollectionOverview />
-              </RequireAuth>)
+    element: (<>
+      <RequireAuth loginPath='/login'>
+        <CollectionOverview />
+      </RequireAuth>
+      <TabNavigation />
+    </>)
   },
   {
     path: '/register',
-    element: <Register/>
+    element: (<>
+      <Register/>
+    </>)
   },
   {
     path: '/login',
-    element: <Login/>
-  },
-  {
-    path: '/logout',
-    element: <Login/>
+    element: (<>
+      <Login/>
+    </>)
   },
   {
     path: '/profile',
-    element: (<RequireAuth loginPath='/login'>
-                <Profile/>
-              </RequireAuth>)
+    element: (<>
+      <RequireAuth loginPath='/login'>
+        <Profile/>
+      </RequireAuth>
+      <TabNavigation />
+    </>)
   },
   {
     path: '/profile/edit',
-    // element: (<RequireAuth loginPath='/login'>
-    //             <Profile/>
-    //           </RequireAuth>)
-    element: <ProfileEdit />
+    element: (<>
+      <RequireAuth loginPath='/login'>
+        <ProfileEdit/>
+      </RequireAuth>
+      <TabNavigation />
+    </>)
   },
   {
     path: '/collections',
-    element: (<RequireAuth loginPath='/login'>
-                <CollectionOverview />
-              </RequireAuth>)
+    element: (<>
+      <RequireAuth loginPath='/login'>
+        <CollectionOverview />
+      </RequireAuth>
+      <TabNavigation />
+    </>)
   },
   {
     path: '/collection/:collectionId',
-    element: (<RequireAuth loginPath='/login'>
-                <CollectionSingle />
-              </RequireAuth>)
+    element: (<>
+      <RequireAuth loginPath='/login'>
+        <CollectionSingle />
+      </RequireAuth>
+      <TabNavigation />
+    </>)
   }
 ])
 
@@ -70,7 +84,6 @@ function App() {
         <Header />
         <RouterProvider router={router} />
       </HeaderProvider>
-      {/* <TabNavigation /> */}
     </AuthProvider>
   </>);
 }
