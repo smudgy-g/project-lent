27b5325e6b75c23b79b89f626632f9b09ccca063
Kevin Kowalski <hello@kevinkowalski.com>
2023-08-21 11:28:21 +0200

    fix: errors on front end

--- client/src/components/collections/CollectionOverview.tsx
+++ client/src/components/collections/CollectionOverview.tsx
@@ -50,28 +50,46 @@ export default function CollectionOverview () {
       {collections && (<>
         <div className="static-collections">
           {collections
-            .filter((collection) => collection.name.toLowerCase() === 'all' || collection.name.toLowerCase() === 'lent out' || collection.name.toLowerCase() === 'reserved' || collection.name.toLowerCase() === 'borrowed')
+            .filter((collection) => (
+              collection.name?.toLowerCase() === 'all'
+              || collection.name?.toLowerCase() === 'lent out'
+              || collection.name?.toLowerCase() === 'reserved'
+              || collection.name?.toLowerCase() === 'borrowed'
+            ))
             .map((collection) => (<>
-                {collection.items[0] && (
-                  <Link to={`/collection/${collection._id}`}>
-                    <div className="static-collection-preview">
-                      <div className="static-collection-image" style={{backgroundImage: `url(${collection.items[0].img_url})`}}></div>
+                  {collection.items && collection.items[0] && (
+                    <Link to={`/collection/${collection._id}`}>
+                      <div className="static-collection-preview">
+                        <div
+                          className="static-collection-image"
+                          style={{backgroundImage: `url(${collection.items[0].img_url})`}}>
+                        </div>
+                        <h2>{collection.name}</h2>
+                      </div>
+                    </Link>
+                  )}
+                  {collection.items && !collection.items[0] && (
+                    <div className="static-collection-preview deactivated">
+                      <div className="static-collection-image deactivated"></div>
                       <h2>{collection.name}</h2>
                     </div>
-                  </Link>
-                )}
-                {!collection.items[0] && (
-                  <div className="static-collection-preview deactivated">
-                    <div className="static-collection-image deactivated"></div>
-                    <h2>{collection.name}</h2>
-                  </div>
-                )}
+                  )}
             </>))
           }
         </div>
       </>)}
       {/* <h1>Collections</h1> */}
-      {collections && <CollectionList collections={collections.filter((collection) => collection.name.toLowerCase() !== 'all' && collection.name.toLowerCase() !== 'lent out' && collection.name.toLowerCase() !== 'reserved' && collection.name.toLowerCase() !== 'borrowed')} />}
+      {collections && (
+        <CollectionList collections={
+          collections
+            .filter((collection) => (
+              collection.name?.toLowerCase() !== 'all'
+              && collection.name?.toLowerCase() !== 'lent out'
+              && collection.name?.toLowerCase() !== 'reserved'
+              && collection.name?.toLowerCase() !== 'borrowed'
+            ))
+        } />
+      )}
     </div>
   </>)
 }
