e827d26eb1e522635893c63b7c21cbeed72ccee5
Adam Griffiths <107414001+adamgriff86@users.noreply.github.com>
2023-08-24 11:54:00 +0200

    update db connect and fly.io toml file

--- server/src/models/_index.ts
+++ server/src/models/_index.ts
@@ -1,5 +1,8 @@
-import { connect } from 'mongoose';
+import { connect  } from 'mongoose';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 import dotenv from 'dotenv';
+
+const connectionString = process.env.DB_URL as string;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -7,10 +10,24 @@ const envFileName = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
 
 dotenv.config({ path: envFileName });
 
-const connectionString = process.env.DB_URL as string;
+const client = new MongoClient(connectionString, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 async function connectDb() {
-  return await connect(connectionString);
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("lentDB").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
 
 export default connectDb;
